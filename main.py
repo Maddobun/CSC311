7ccf50b9fa12ae832b371912ce4255bcdd260979
Leo <yifu.zhu@mail.utoronto.ca>
2022-09-28 19:04:58 -0400

    finished Q2d

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import models.hm1_code as dtree
+from sklearn.tree import DecisionTreeClassifier
+from typing import List
+import numpy as np
 
 if __name__ == "__main__":
-    train, val, test = dtree.load_data()
-    dtree.select_model(train, val)
+    train, val, test, feature_names = dtree.load_data()
+    best = dtree.select_model(train, val, feature_names)
+    dtree.test_model(best, test)
+
+    # get the index of feature named trump
+    top_5 = [("trump", 0.056),
+             ("trumps", 0.162),
+             ("hillary", 0.069),
+             ("trump", 0.051),
+             ("the", 0.325)]
+
+    for entry in top_5:
+        dtree.compute_information_gain(train[0].toarray(), train[1], entry, feature_names)
+
--- models/hm1_code.py
+++ models/hm1_code.py
@@ -1,3 +1,4 @@
+import scipy.sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
@@ -20,12 +21,12 @@ def load_data() -> tuple:
     with open(path[0], "r") as f:
         for line in f:
             data.append(line)
-            labels.append([1])
+            labels.append(["real"])
 
     with open(path[1], "r") as f:
         for line in f:
             data.append(line)
-            labels.append([-1])
+            labels.append(["fake"])
 
     data, labels = np.array(data), np.array(labels)
 
@@ -34,13 +35,12 @@ def load_data() -> tuple:
 
     # split data
     data_train, data_test, label_train, label_test = train_test_split(data, labels, test_size=0.3, random_state=42)
-
     data_val, data_test, label_val, label_test = train_test_split(data_test, label_test, test_size=0.5, random_state=42)
 
-    return [data_train, label_train], [data_val, label_val], [data_test, label_test]
+    return (data_train, label_train), (data_val, label_val), (data_test, label_test), vectorizer.get_feature_names_out()
 
 
-def select_model(train: list, val: list) -> None:
+def select_model(train: list, val: list, feature_names: list) -> DecisionTreeClassifier:
     """
     Train decision tree models with different hyperparameters and print out the accuracy for each model
     """
@@ -56,30 +56,46 @@ def select_model(train: list, val: list) -> None:
 
     # train a model for each combination of hyperparameters
     for depth, crit in parameters:
-        clf = DecisionTreeClassifier(max_depth=depth, criterion=crit)
+        clf = DecisionTreeClassifier(max_depth=depth, criterion=crit, random_state=42)
         clf.fit(train[0], train[1])
         trees.append(clf)
 
-    # print out the accuracy for each model
+    # compute accuracy
+    accuracies = []
     for tree in trees:
+        accuracies.append(tree.score(val[0], val[1]))
+
+    # print out accuracy
+    for i, tree in enumerate(trees):
         print(f"Accuracy for the model trained with max depth {tree.get_depth()} and criterion {tree.criterion}:"
-              f" {tree.score(val[0], val[1])}")
-        print()
+              f" {accuracies[i]}")
+
+    # print the hyperparameters for the model with the highest accuracy
+    print(
+        f'The model with the highest accuracy is trained with max depth'
+        f' {trees[accuracies.index(max(accuracies))].get_depth()} '
+        f'and criterion {trees[accuracies.index(max(accuracies))].criterion}')
 
     # plot the accuracy for each model as function of max depth
-    plot_accuracy(trees, val)
+    plot_accuracy(trees, accuracies)
+
+    # visualize the decision tree with the highest accuracy
+    visualize_tree(trees[accuracies.index(max(accuracies))], feature_names)
+
+    # for testing
+    return trees[accuracies.index(max(accuracies))]
 
 
-def plot_accuracy(trees: List[DecisionTreeClassifier], val: list) -> None:
+def plot_accuracy(trees: List[DecisionTreeClassifier], accuracies: list) -> None:
     """
     Plot the accuracy for each model as function of max depth.
 
     :param trees: list of decision tree models
-    :param val: 2d array where the first column is the validation data and the second column is the labels
+    :param accuracies: list of accuracies for each model
+
     """
 
-    # generate a list of accuracies for each model
-    accuracies = [tree.score(val[0], val[1]) for tree in trees]
+    # generate a list of depth for each model
     depths = [tree.get_depth() for tree in trees]
 
     # plot the accuracy for each model as function of max depth
@@ -90,21 +106,89 @@ def plot_accuracy(trees: List[DecisionTreeClassifier], val: list) -> None:
     plt.show()
 
 
+def visualize_tree(tree: DecisionTreeClassifier, feature_names: list) -> None:
+    """
+    Visualize the decision tree with the highest accuracy.
+
+    :param tree: decision tree model
+    :param feature_names: list of feature names
+    """
+
+    from sklearn.tree import export_graphviz
+    import graphviz
+
+    dot_data = export_graphviz(tree, out_file=None,
+                               feature_names=feature_names,
+                               class_names=["real", "fake"],
+                               max_depth=2)
+    graph = graphviz.Source(dot_data)
+    graph.render("tree")
 
 
+def test_model(tree: DecisionTreeClassifier, test: tuple) -> None:
+    """
+    Test the model on the test set and print out the accuracy.
+
+    :param tree: decision tree model
+    :param test: test set
+    """
+
+    print(f"Accuracy on test set: {tree.score(test[0], test[1])}")
 
 
+def compute_information_gain(train_data: np.ndarray,
+                             train_label: np.ndarray,
+                             feature: (str, float),
+                             feature_names) -> None:
+    """
+    Compute the information gain on training set for the given feature
 
+    :param train_data: training set
+    :param train_label: training labels
+    :param feature: the feature to split, given as the name of the feature
+    :param feature_names: list of feature names
+    """
 
+    # get the index of the feature
+    feature_index = np.where(feature_names == feature[0])
+    train_label = np.where(train_label == "real", 1.0, 0.0)
 
+    # merge the training set and training labels
+    data = np.c_[train_data, train_label]
 
+    # count the number of real and fake news in the training set
+    real_count = np.count_nonzero(data[:, -1] == 1)
+    fake_count = np.count_nonzero(data[:, -1] == 0)
 
+    # compute initial entropy
+    initial_entropy = - (real_count / (real_count + fake_count)) * np.log2(real_count / (real_count + fake_count)) - (
+            fake_count / (real_count + fake_count)) * np.log2(fake_count / (real_count + fake_count))
 
+    # split the data based on the given feature and its value
+    cond = data[:, feature_index[0]] < feature[1]
+    left_index = np.where(cond)
+    right_index = np.where(~cond)
+    left = data[left_index[0]]
+    right = data[right_index[0]]
 
+    # count the number of real and fake news in the left and right split
+    left_real_count = np.count_nonzero(left[:, -1] == 1)
+    left_fake_count = np.count_nonzero(left[:, -1] == 0)
+    right_real_count = np.count_nonzero(right[:, -1] == 1)
+    right_fake_count = np.count_nonzero(right[:, -1] == 0)
 
+    left_entropy = - left_real_count / left.shape[0] * np.log2(left_real_count / left.shape[0]) \
+                   - left_fake_count / left.shape[0] * np.log2(left_fake_count / left.shape[0])
+    right_entropy = - right_real_count / right.shape[0] * np.log2(right_real_count / right.shape[0]) \
+                    - right_fake_count / right.shape[0] * np.log2(right_fake_count / right.shape[0])
 
+    expected_conditional_entropy = left.shape[0] / data.shape[0] * left_entropy + \
+                                   right.shape[0] / data.shape[0] * right_entropy
 
+    # compute information gain
 
+    information_gain = initial_entropy - expected_conditional_entropy
 
+    print(f"Information gain for feature {feature[0]} with value {feature[1]}: {information_gain}")
 
 
